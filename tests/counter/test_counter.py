3ad9f5f139c5397c8ef647aadb1db23e3da59be0
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-04-28 10:13:40 -0300

    feat: req10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,19 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    numbCountPy = count_ocurrences('data/jobs.csv', 'Python')
+
+    assert numbCountPy == 1639
+
+    numbCountp2 = count_ocurrences('data/jobs.csv', 'python')
+
+    assert numbCountp2 == 1639
+
+    numbCountJv = count_ocurrences('data/jobs.csv', 'Javascript')
+
+    assert numbCountJv == 122
+
+    numbCountjv2 = count_ocurrences('data/jobs.csv', 'javascript')
+
+    assert numbCountjv2 == 122
